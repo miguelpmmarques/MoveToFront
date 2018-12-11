20d8493a2c51399aa320a0b884b324f37590da35
Miguel Marques <38664523+miguelpmmarques@users.noreply.github.com>
2018-12-11 15:38:00 +0000

    Add files via upload

---
+++ MoveToFront.m
@@ -0,0 +1,14 @@
+function output = MTFll(s)
+
+    alfabeto = unique(s);
+    output = zeros(length(s),1);
+    for n=1:length(s)
+        simbolo = s(n);
+        index = find(alfabeto==simbolo,1);
+        alfabeto = alfabeto([1:index-1,index+1:end]);
+        alfabeto(2:end+1) = alfabeto;
+        alfabeto(1) = simbolo;
+        output(n) = index;
+    end
+    
+end 
